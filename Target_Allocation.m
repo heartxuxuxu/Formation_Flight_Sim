4f8e2dbc252a9f3aaacde6dc3092f2e06671d9a4
heartxuxuxu <heyheartdream>
2018-01-22 20:47:42 +0800

    improve the target allocation from CSY

--- Target_Allocation.m
+++ Target_Allocation.m
@@ -1,5 +1,10 @@
 function [goal]=Target_Allocation(goal,quad_init_x,quad_init_y,num,target_num,goal_series,temp_goal,mirror_dis)
 mirror_count=0;
+row=0;
+line=0;
+temp_sum_dis=0;
+sum_dis=[0;0;0;0];
+limited_init_distance=7;%?????????
 quad_init=[quad_init_x quad_init_y];
 distance=[sqrt((quad_init(1,1)-goal(1,1))^2+(quad_init(1,2)-goal(1,2))^2) sqrt((quad_init(1,1)-goal(2,1))^2+(quad_init(1,2)-goal(2,2))^2) sqrt((quad_init(1,1)-goal(3,1))^2+(quad_init(1,2)-goal(3,2))^2) sqrt((quad_init(1,1)-goal(4,1))^2+(quad_init(1,2)-goal(4,2))^2);
           sqrt((quad_init(2,1)-goal(1,1))^2+(quad_init(2,2)-goal(1,2))^2) sqrt((quad_init(2,1)-goal(2,1))^2+(quad_init(2,2)-goal(2,2))^2) sqrt((quad_init(2,1)-goal(3,1))^2+(quad_init(2,2)-goal(3,2))^2) sqrt((quad_init(2,1)-goal(4,1))^2+(quad_init(2,2)-goal(4,2))^2);
@@ -35,7 +40,22 @@ for k=1:num
         if (mirror_dis(k,j)==1) || (mirror_dis(k,j)==0)
             goal_series(k)=j;
         end
+        if mirror_dis(k,j)==0
+            row=k;
+            line=j;
+        end
     end
+end
+if distance(row,line)>limited_init_distance
+    for k=1:num%????????
+        sum_dis(k)=(distance(k,line)+distance(row,goal_series(k)))-(distance(k,goal_series(k))+distance(row,line));
+        if temp_sum_dis >= sum_dis(k)
+            temp_sum_dis=sum_dis(k);
+            second_row=k;
+        end
+    end
+    goal_series(row)=goal_series(second_row);
+    goal_series(second_row)=line;
 end
 for k=1:target_num
     temp_goal(k,:)=goal(goal_series(k),:);
