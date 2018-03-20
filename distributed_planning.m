f6b646d93ee37744764efe788023fa466f46dbdb
heartxuxuxu <heyheartdream@163.com>
2018-03-20 20:57:14 +0800

    asss

--- distributed_planning.m
+++ distributed_planning.m
@@ -12,10 +12,12 @@
 % -------------------------------------------------------------------------
 
 function [x,traj] = distributed_planning(x,Kinematic,goal,evalParam,obstacle,obstacleR)
+
     % DWA参数输入
     [u,traj]=DynamicWindowApproach(x,Kinematic,goal,evalParam,obstacle,obstacleR);
 %     u(1,1)=u(1,1)+0.01*rand(1,1);
     x=f(x,u);% 机器人移动到下一个时刻
+    
 end
 
 
